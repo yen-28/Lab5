284e4624fdfbb184a4328d9670064cda53459a03
timos <timoshinartoym@gmail.com>
2026-03-02 00:10:47 +0300

    main, instrument types

---
+++ src/main/java/ru/itmo/zhmeh/Main.java
@@ -0,0 +1,7 @@
+package ru.itmo.zhmeh;
+
+import ru.itmo.zhmeh.things.*;
+public class Main {
+    public static void main(String[] args){
+    }
+}
--- src/main/java/ru/itmo/zhmeh/things/InstrumentType.java
+++ src/main/java/ru/itmo/zhmeh/things/InstrumentType.java
@@ -1,4 +1,9 @@
 package ru.itmo.zhmeh.things;
 
 public enum InstrumentType {
+    PH_METER,
+    BALANCE,
+    SPECTROPHOTOMETER,
+    CONDUCTIVITY_METER,
+    THERMOMETER
 }
