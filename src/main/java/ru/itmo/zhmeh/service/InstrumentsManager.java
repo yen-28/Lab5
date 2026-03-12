784d6fff18d3d88cfb5d1bd0e1d23dee5226d405
timos <timoshinartoym@gmail.com>
2026-03-12 21:25:28 +0300

    CollectionManager: addInstrument with validator

--- src/main/java/ru/itmo/zhmeh/service/InstrumentsManager.java
+++ src/main/java/ru/itmo/zhmeh/service/InstrumentsManager.java
@@ -6,6 +6,7 @@ import ru.itmo.zhmeh.things.InstrumentType;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 
 public class InstrumentsManager {
@@ -17,11 +18,29 @@ public class InstrumentsManager {
     public long generateId() {
         return nextId++;
     }
+    /*
+    Instrument :: getInventoryNumber эквивалентно лямбда выражению:
+    inst -> inst.getInventoryNumbe
+    - оно берёт каждый новый "инст" и для него выполняет метод
+     */
 
-    public void addInstrument(String ownerUsername, String name, InstrumentType type, String inventoryNumber, String location, InstrumentStatus status ){
-
-
-      //  instruments.put(generateId(), instrument);
+    private void validateInventoryNumber(String number) {
+        if (number == null || number.isEmpty()) { //пустые и налл не проверяем
+            return;
+        }
+        boolean exist = instruments.values().stream() //создали конвейер
+                .map(Instrument::getInventoryNumber) //для каждого выбрали инвентори намбер, создали
+                .filter(Objects::nonNull) //отбросили налл
+                .anyMatch(number::equals); //проверка на принадлежность
+        if (exist) {
+            throw new IllegalArgumentException("Инвентарный номер: " + number + " занят");
+        }
+    }
+    public void addInstrument(String ownerUsername, String name, InstrumentType type, String inventoryNumber, String location, InstrumentStatus status){
+        validateInventoryNumber(inventoryNumber);
+        long id = generateId();
+        Instrument instrument = new Instrument(id, ownerUsername, name, type, inventoryNumber, location, status);
+        instruments.put(id, instrument);
     }
 
 
