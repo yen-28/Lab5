48e1958f0e49394c505f364c15c6b2b59a07db47
timos <timoshinartoym@gmail.com>
2026-03-25 19:16:48 +0300

    fromString

--- src/main/java/ru/itmo/zhmeh/domain/MaintenanceType.java
+++ src/main/java/ru/itmo/zhmeh/domain/MaintenanceType.java
@@ -1,6 +1,20 @@
 package ru.itmo.zhmeh.domain;
 
+import ru.itmo.zhmeh.validation.FieldValidator;
+import ru.itmo.zhmeh.validation.StringValidationType;
+
 public enum MaintenanceType {
     SERVICE,
-    REPAIR
+    REPAIR;
+
+    public static MaintenanceType fromString(String type){
+        FieldValidator.validateIsStringEmpty(type, StringValidationType.MAINTEN_TYPE);
+        for (MaintenanceType tp : MaintenanceType.values()){
+            if(tp.name().equalsIgnoreCase(type)){
+                return tp;
+            }
+        }
+        throw new IllegalArgumentException("Ошибка: неверный тип обслуживания: " + type + ", выберете из списка");
+
+    }
 }
