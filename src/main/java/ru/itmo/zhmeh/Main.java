e82a57987110c3d55821c29535d3bd07301d58e8
timos <timoshinartoym@gmail.com>
2026-04-02 23:09:09 +0300

    Main приведён в порядок

--- src/main/java/ru/itmo/zhmeh/Main.java
+++ src/main/java/ru/itmo/zhmeh/Main.java
@@ -1,11 +1,39 @@
 package ru.itmo.zhmeh;
 
-import ru.itmo.zhmeh.service.InstrumentsManager;
+import ru.itmo.zhmeh.cli.*;
+import ru.itmo.zhmeh.service.*;
+
+import java.util.Scanner;
 
 public class Main {
+    public static final InstrumentsManager instrumentsManager = new InstrumentsManager();
     public static void main(String[] args){
-        InstrumentsManager manager = new InstrumentsManager();
-        manager.addNew(null, "gbsdv", "PH_METER", "123", "баня", "active");
-        System.out.println(manager.getById(1));
+        Scanner scanner = new Scanner(System.in);  //пока не разбирался
+
+        CommandManager commandManager = new CommandManager(); // добавляю команды в список
+        commandManager.addCommand("inst_add", new InstAdd());
+        commandManager.addCommand("help", new Help()); //TODO сделать хелп
+
+
+
+        while (true){
+            String input = scanner.nextLine();
+            try {
+                if (input.equalsIgnoreCase("exit")){
+                    System.out.println("ВЫХОД. СПАСИБО ЗА ИСПОЛЬЗОВАНИЕ!");
+                    break;
+                }
+
+
+
+                if (input.isEmpty()) continue;
+
+            }
+            catch (Exception e){
+                System.out.println("Ошибка: " + e.getMessage());
+            }
+
+
+        }
     }
 }
