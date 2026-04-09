17657083447e62a58f6958110a87a6fa282c075f
timos <timoshinartoym@gmail.com>
2026-04-09 09:01:21 +0300

    Всё-таки сделал инвайромент

---
+++ src/main/java/ru/itmo/zhmeh/cli/Environment.java
@@ -0,0 +1,33 @@
+package ru.itmo.zhmeh.cli;
+
+import ru.itmo.zhmeh.service.CalibrationManager;
+import ru.itmo.zhmeh.service.InstrumentsManager;
+import ru.itmo.zhmeh.service.MaintenanceManager;
+
+import java.lang.reflect.Member;
+
+public final class Environment {
+    private final MyReader reader;
+    private final CommandManager commandManager;
+    private final InstrumentsManager instrumentsManager;
+    private final CalibrationManager calibrationManager;
+    private final MaintenanceManager maintenanceManager;
+
+
+    private CommandManager addCommands(CommandManager commandManager){
+        commandManager.addCommand(InstAdd.getName(), new InstAdd());
+        commandManager.addCommand(Help.getName(), new Help());
+
+        return commandManager;
+
+    }
+
+
+    public Environment(MyReader reader) {
+        this.reader = reader;
+        this.commandManager = addCommands(new CommandManager());
+        this.instrumentsManager = new InstrumentsManager();
+        this.calibrationManager = new CalibrationManager(instrumentsManager);
+        this.maintenanceManager = new MaintenanceManager(instrumentsManager);
+    }
+}
